d56cd4ce35f350158201d120bd1fbc69c064f3c9
飒然Hang <superhj1987@126.com>
2014-11-04 13:53:37 +0800

    modify bodyparser usage

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var path = require('path');
 var fs = require('fs');
 var express = require('express');
 var errorHandler = require('errorhandler');
-var _ = require('lodash');
 var bodyParser = require('body-parser');
 require('log4js').configure(path.join(__dirname, 'config/log4j.json'),{});
 
@@ -24,7 +23,10 @@ var app = express();
 app.set("env", config.env);
 
 app.use(require('response-time')());
-app.use(require('body-parser')({uploadDir: config.upload_dir}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(require('method-override')());
 
 webFilter.setFilter(app);
@@ -32,11 +34,10 @@ webFilter.setFilter(app);
 app.use('/public',express.static(staticDir));
 //require('./models')(app);
 
-app.use(errorHandler({ dumpExceptions: true, showStack: true }));
+if(config.debug){
+   app.use(errorHandler({ dumpExceptions: true, showStack: true }));
 
-_.extend(app.locals, {
-  config: config
-});
+}
 
 // routes
 routes(app);
